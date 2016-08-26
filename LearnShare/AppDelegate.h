48be41d1895e10176bd78a5757ef566f7ef18a99
James <James@ricky.local>
2016-08-26 10:23:46 +0800

    first

---
+++ LearnShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnShare
+//
+//  Created by James on 16/8/26.
+//  Copyright © 2016年 james. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearnShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearnShare
+//
+//  Created by James on 16/8/26.
+//  Copyright © 2016年 james. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
