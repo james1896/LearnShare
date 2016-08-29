d9a52421cadcce02110a6f30f0a6e225f38f83b6
James <James@ricky.local>
2016-08-29 16:41:32 +0800

    add

---
+++ LearnShare/Main/学习/view/LearnCollectionReusableView.h
@@ -0,0 +1,13 @@
+//
+//  LearnCollectionReusableView.h
+//  LearnShare
+//
+//  Created by James on 16/8/29.
+//  Copyright © 2016年 james. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LearnCollectionReusableView : UICollectionReusableView
+
+@end
---
+++ LearnShare/Main/学习/view/LearnCollectionReusableView.m
@@ -0,0 +1,13 @@
+//
+//  LearnCollectionReusableView.m
+//  LearnShare
+//
+//  Created by James on 16/8/29.
+//  Copyright © 2016年 james. All rights reserved.
+//
+
+#import "LearnCollectionReusableView.h"
+
+@implementation LearnCollectionReusableView
+
+@end
