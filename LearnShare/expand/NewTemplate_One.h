d5f0352a1adc7dbdb7388564611515231ddb2d75
James <James@ricky.local>
2016-09-01 05:45:21 +0800

    aa

---
+++ LearnShare/expand/NewTemplate_One.h
@@ -0,0 +1,13 @@
+//
+//  NewTemplate_One.h
+//  LearnShare
+//
+//  Created by James on 16/9/1.
+//  Copyright © 2016年 james. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewTemplate_One : UIView
+
+@end
---
+++ LearnShare/expand/NewTemplate_One.m
@@ -0,0 +1,21 @@
+//
+//  NewTemplate_One.m
+//  LearnShare
+//
+//  Created by James on 16/9/1.
+//  Copyright © 2016年 james. All rights reserved.
+//
+
+#import "NewTemplate_One.h"
+
+@implementation NewTemplate_One
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
