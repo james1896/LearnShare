06bc197651b9a5f80d3cc7d7d1bf866204ccec2b
James <James@ricky.local>
2016-08-31 11:10:26 +0800

    ss

---
+++ LearnShare/other/Category/UIView+Attribute.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Attribute.h
+//  LearnShare
+//
+//  Created by James on 16/8/30.
+//  Copyright © 2016年 james. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Attribute)
+
+@end
---
+++ LearnShare/other/Category/UIView+Attribute.m
@@ -0,0 +1,13 @@
+//
+//  UIView+Attribute.m
+//  LearnShare
+//
+//  Created by James on 16/8/30.
+//  Copyright © 2016年 james. All rights reserved.
+//
+
+#import "UIView+Attribute.h"
+
+@implementation UIView (Attribute)
+
+@end
