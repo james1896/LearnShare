cd45a6f8b65fb774ea4385f033da0abc84e3a6a8
James <James@ricky.local>
2016-08-31 13:36:32 +0800

    ss

---
+++ LearnShare/Main/我的/controller/AboutUsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutUsViewController.h
+//  LearnShare
+//
+//  Created by James on 16/8/31.
+//  Copyright © 2016年 james. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutUsViewController : UIViewController
+
+@end
